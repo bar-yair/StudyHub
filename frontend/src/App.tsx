c4e5af8971a5835a58c34f8abdb8b97bc0cceb3c
בר יאיר <baryair12@gmail.com>
2025-01-23 17:38:14 +0200

    Your commit message here

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 import NavBar from './components/NavBar/NavBar'
 import Home from './components/home/home'
 import Login from './components/login/login'
 import Chatupload from './components/chatupload/chatupload'
+import Register from './components/register/register'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div style={{ width: '100%' }}>
-      {/* <NavBar /> */}
-      <Chatupload />
+      <NavBar />
+      {/* <Home/> */}
+      {/* <Chatupload /> */}
       {/* <Login /> */}
+      {/* <Register /> */}
     </div>
   )
 }
